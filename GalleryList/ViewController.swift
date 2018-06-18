4ca00d73b34b46e984cbadf21d64580eac4b3280
Mykola Savoniuk <mykola.savoniuk@idapgroup.com>
2018-06-18 17:36:12 +0300

    Initial commit

---
+++ GalleryList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GalleryList
+//
+//  Created by Mykola Savoniuk on 6/18/18.
+//  Copyright © 2018 Mykola Savoniuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
