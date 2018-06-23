2b1df1728904ddcd297ba3c49fe04232a48e4fb9
Mykola Savoniuk <mykola.savoniuk@idapgroup.com>
2018-06-23 15:26:16 +0300

    Added custom subscript for objects

--- GalleryList/Source/UI/ListViewController/ListViewController.swift
+++ GalleryList/Source/UI/ListViewController/ListViewController.swift
@@ -7,14 +7,45 @@
 //
 
 import UIKit
+import Foundation
+
+struct Workers {
+    var managers = [Manager]()
+    var workers = [Worker]()
+    var bookkeepers = [Bookkeeper]()
+    
+    subscript (index: Int) -> [ParentWorker] {
+        var parentWorkers = [ParentWorker]()
+        
+        switch index {
+        case 0:
+            parentWorkers = self.managers
+        case 1:
+            parentWorkers = self.workers
+        case 2:
+            parentWorkers = self.bookkeepers
+        default:
+            print("default")
+            
+        }
+        
+        return parentWorkers
+    }
+}
 
 class ListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     // MARK: - Properties
     
+    @IBOutlet var rootView: ListView!
+    
     var managers = [Manager]()
     var workers = [Worker]()
     var bookkeepers = [Bookkeeper]()
+    
+    let workerss = Workers()
+    
+    let headerTitles = ["Managers", "Workers", "Bookkeepers"]
     
     // MARK: - ViewController lifecycle
     
@@ -23,20 +54,42 @@ class ListViewController: UIViewController, UITableViewDelegate, UITableViewData
         
         self.configureView()
         self.configureNavigationItem()
+        //String(describing: type(of: self.workerss.workers))
     }
     
     // MARK: - UITableViewDataSource
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return 5
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        let cell = UITableViewCell()
+        
+        switch indexPath.section {
+        case 1:
+            cell.textLabel?.text = self.managers[indexPath.section].name
+        case 2:
+            cell.textLabel?.text = self.workers[indexPath.section].name
+        case 3:
+            cell.textLabel?.text = self.bookkeepers[indexPath.section].name
+        default:
+            print("default")
+        }
+
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return self.headerTitles[section]
     }
     
     // MARK: - Private
-    
+
     private func configureView() {
         self.navigationItem.title = VCTitles.list.rawValue
     }
@@ -49,14 +102,16 @@ class ListViewController: UIViewController, UITableViewDelegate, UITableViewData
     @objc private func showItemVC() {
         let controller = ItemViewController(option: .new)
         
-        controller.workerHandler = { worker in
+        controller.workerHandler = { [weak self] worker in
             if type(of: worker) == Worker.self {
-                self.workers.append(worker as! Worker)
+                self?.workers.append(worker as! Worker)
             } else if type(of: worker) == Manager.self {
-                self.managers.append(worker as! Manager)
+                self?.managers.append(worker as! Manager)
             } else if type(of: worker) == Bookkeeper.self {
-                self.bookkeepers.append(worker as! Bookkeeper)
+                self?.bookkeepers.append(worker as! Bookkeeper)
             }
+            
+            self?.rootView.tableView.reloadData()
             
             controller.dismiss(animated: true, completion: nil)
         }
---
+++ GalleryList/Source/UI/ListViewController/View/ListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ListTableViewCell.swift
+//  GalleryList
+//
+//  Created by Mykola Savoniuk on 6/23/18.
+//  Copyright © 2018 Mykola Savoniuk. All rights reserved.
+//
+
+import UIKit
+
+class ListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
